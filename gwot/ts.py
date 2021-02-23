45b0022208d4bead4d2f685280676da4d986c234
Stephen Zhang <syz@math.ubc.ca>
2021-02-23 14:14:19 -0800

    added more documentation

--- gwot/ts.py
+++ gwot/ts.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 class TimeSeries():
+    """Base class for time-series dataset
+    
+    :param x: `np.array` of observed datapoints. 
+    :param dt: `np.array` of time increments `t[i+1] - t[i]`.
+    :param t_idx: `np.array` of time indices for each datapoint in `x`. 
+    :param D: diffusivity 
+    """
     def __init__(self, x, dt, t_idx, D = None):
         self.x = x
         self.t_idx = t_idx
